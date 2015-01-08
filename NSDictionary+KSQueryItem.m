45f3b614b8cb295fc8757c2b17fd222d414d629b
Keith Smiley <keithbsmiley@gmail.com>
2015-01-07 23:50:31 -0800

    Add debug logging for type failures

--- NSDictionary+KSQueryItem.m
+++ NSDictionary+KSQueryItem.m
@@ -7,11 +7,17 @@
     NSMutableArray *queryItems = [NSMutableArray new];
     for (NSString *key in self.allKeys) {
         if (![key isKindOfClass:[NSString class]]) {
+#ifdef DEBUG
+            NSLog(@"%s '%@' is not a string and therefore is ignored", __PRETTY_FUNCTION__, key);
+#endif
             continue;
         }
 
         NSString *value = self[key];
         if (![value isKindOfClass:[NSString class]]) {
+#ifdef DEBUG
+            NSLog(@"%s '%@' is not a string and therefore is ignored", __PRETTY_FUNCTION__, value);
+#endif
             continue;
         }
 
